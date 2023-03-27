294b1ca5c95978882e0084ef287559610ad9c69a
Kathy_Laip <ekaterinaslapnikova@Ekaterinas-MacBook-Air.local>
2023-03-27 23:28:57 +0300

    new pages for manager

--- momentomori/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import LogIn from './components/LogIn';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <LogIn/>
-);
